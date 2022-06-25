f3960a612098f579297281fb7d35752fb50c608b
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-24 21:14:11 -0700

    Add better logging

--- src/main.c
+++ src/main.c
@@ -3,25 +3,29 @@
 #include <string.h>
 #include <time.h>
 
-#define SEARCH_SIZE 1000
+#define SEARCH_SIZE 10000
 
 char ZEROARRAY[SEARCH_SIZE] = {0};
 
 void ray_detected() {
-    time_t rawtime;
-    struct tm *timeinfo;
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    char *time_str = asctime(timeinfo);
+    time_t mytime = time(NULL);
+    char *time_str = ctime(&mytime);
     time_str[strlen(time_str) - 1] = '\0';
 
-    printf("%s - ", time_str);
-    cprint("A cosmic ray has been detected!\n", FG_GREEN);
+    cprint("A cosmic ray has been detected! ", FG_GREEN);
+    printf("%s\n", time_str);
+}
+
+void start() {
+    time_t mytime = time(NULL);
+    char *time_str = ctime(&mytime);
+    time_str[strlen(time_str) - 1] = '\0';
+
+    printf("Cosmic Ray Detector... %s\n", time_str);
 }
 
 int main() {
-    printf("Cosmic Ray Detector...\n");
+    start();
 
     while (1) {
         for (int i = 0; i < SEARCH_SIZE; i++) {
